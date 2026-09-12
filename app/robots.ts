d0adb30a90af9f4381750f231261b52572566218
gemkam <gemkam@gmail.com>
2026-09-12 06:43:29 +0400

    Update base URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://basera-bazar-lac.vercel.app';
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://1bazarify.store';
 
   return {
     rules: [
